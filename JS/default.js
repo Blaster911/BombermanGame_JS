41e594afca72b3edfa1a7caf519fd70778f1d1c7
Blaster911 <romain588@live.fr>
2022-04-18 21:02:29 +0200

    resize img

--- JS/default.js
+++ JS/default.js
@@ -1,10 +1,28 @@
 let containerGame = document.getElementById("container_game");
-let terrain = document.createElement("div");
-terrain.style.width = "600px";
-terrain.style.height = "600px";
-terrain.style.position = "relative";
-terrain.style.margin = "0 auto";
-terrain.style.backgroundRepeat = "repeat";
-terrain.style.backgroundSize = "10% 10%";
-terrain.style.backgroundImage = "url('img/ground.svg')";
-containerGame.appendChild(terrain);
+let lot = document.createElement("div");
+lot.classList.add("lot")
+lot.style.width = "100vw";
+lot.style.height = "100vh";
+lot.style.position = "relative";
+lot.style.margin = "0 auto";
+lot.style.backgroundRepeat = "repeat";
+console.log(window.resizeBy);
+if (window.innerWidth <= 1240) {
+    lot.style.backgroundSize = "10% 10%";
+}
+if (window.innerWidth >= 1240) {
+    lot.style.backgroundSize = "5% 10%";
+}
+
+window.addEventListener('resize', () => {
+    console.log(window.innerWidth);
+    if (window.innerWidth <= 1240) {
+        lot.style.backgroundSize = "10% 10%";
+    }
+    if (window.innerWidth >= 1240) {
+        lot.style.backgroundSize = "5% 10%";
+    }
+});
+
+lot.style.backgroundImage = "url('img/ground.svg')";
+containerGame.appendChild(lot);
